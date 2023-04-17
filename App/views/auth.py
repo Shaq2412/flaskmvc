d4df093cc3d9f36042182140a81910293a0f0e3c
Shaq2412 <shaquim.pierre@my.uwi.edu>
2023-04-17 20:25:51 +0000

    first

--- App/views/auth.py
+++ App/views/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
-from flask_login import login_required, login_user, current_user, logout_user
+from flask_login import LoginManager, login_required, login_user, current_user, logout_user
 
 from.index import index_views
 
@@ -11,6 +11,8 @@ from App.controllers import (
 )
 
 auth_views = Blueprint('auth_views', __name__, template_folder='../templates')
+
+
 
 '''
 Page/Action Routes
@@ -34,14 +36,15 @@ def login_action():
     user = login(data['username'], data['password'])
     if user:
         login_user(user)
-        return 'user logged in!'
-    return 'bad username or password given', 401
+        return redirect('/home')
+    flash("Invalid Username or Password")
+    return redirect('/')
 
 @auth_views.route('/logout', methods=['GET'])
+@login_required
 def logout_action():
-    data = request.form
-    user = login(data['username'], data['password'])
-    return 'logged out!'
+    logout_user()
+    return redirect('/')
 
 '''
 API Routes
--- App/views/index.py
+++ App/views/index.py
@@ -1,11 +1,17 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
 from App.models import db
 from App.controllers import create_user
+from flask_login import LoginManager, current_user, login_user, login_required, logout_user
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
 @index_views.route('/', methods=['GET'])
 def index_page():
+    return render_template('login.html')
+
+@index_views.route('/home', methods=['GET'])
+@login_required
+def index_home():
     return render_template('index.html')
 
 @index_views.route('/init', methods=['GET'])
